6f84a1ba80858c1f9c4b0bfa2a3ecdee4a51a700
Yohana Christy <fransiskayohanachristy@gmail.com>
2022-07-05 11:08:58 +0700

    Memasukan script streamlit

--- visualisasi.py
+++ visualisasi.py
@@ -1,6 +1,10 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st 
+import plotly.express as px
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
+
 
 st.write("""
 # Aplikasi Yahoo Finance
@@ -9,9 +13,33 @@ st.write("""
 Berikut ini adalah data closing price dan volume dari Google. 
 """)
 
-ticker = 'GOOGL' 
+ticker = st.selectbox(
+    "Ticker Perusahaan",
+    options = [ "ANTM", "AAPL", "MSFT", "GOOG", "AMZN"]
+) 
 tickerData = yf.Ticker(ticker)
-tickerDF = tickerData.history(period='1d', start='2022-01-01', end='2022-05-30')
+
+jumlah_hari = timedelta(days=-30)
+
+
+tgl_mulai = st.date_input(
+    "Mulai dari tanggal"
+    value=date.today()+jumlah_hari
+)
+
+tgl_akhir = st.date_input(
+    "Hingga"
+    value=date.today()
+)
+
+
+# hard coded
+tickerDF = tickerData.history(
+    period='1d',
+    start='2022-01-01',
+    end=srt(tgl_akhir)
+)
+
 
 st.write(type(tickerDF))
 
